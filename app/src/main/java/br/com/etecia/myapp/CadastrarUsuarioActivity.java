96b59b9c42159e75ace31d84bddafb94967c7170
richardfreiree <richardzzfreirezz@gmail.com>
2025-10-30 19:34:44 -0300

    sobe sobe

--- app/src/main/java/br/com/etecia/myapp/CadastrarUsuarioActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CadastrarUsuarioActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class CadastrarUsuarioActivity extends AppCompatActivity {
 
+
+     MaterialButton btnCadastrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,14 @@ public class CadastrarUsuarioActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(), CadastrarUsuarioActivity.class));
+            }
         });
     }
 }
