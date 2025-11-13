62c4598b361657335cbfff3948aac29aa4332c8c
isaquesousa22 <isaqueluzsousa83@gmail.com>
2025-11-13 20:59:27 -0300

    sobe sobe

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class MenuPrincipalActivity extends AppCompatActivity {
 
+    MaterialCardView card1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,14 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        card1 = findViewById(R.id.card1);
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ComponentesActivity.class));
+            }
         });
     }
 }
