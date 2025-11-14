c872a6eea66c48bd43b81b98e4b4b7b6a47f3910
isaquesousa22 <isaqueluzsousa83@gmail.com>
2025-11-13 21:50:28 -0300

    sobe sobe

---
+++ app/src/main/java/br/com/etecia/myapp/MouseFragment.java
@@ -0,0 +1,22 @@
+package br.com.etecia.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MouseFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mouse, container, false);
+        return view;
+    }
+}
