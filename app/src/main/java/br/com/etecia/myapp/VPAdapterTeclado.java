d03913663f6f59e677a37ee290605608ace93253
isaquesousa22 <isaqueluzsousa83@gmail.com>
2025-11-13 22:03:21 -0300

    sobe sobe

--- app/src/main/java/br/com/etecia/myapp/VPAdapterTeclado.java
+++ app/src/main/java/br/com/etecia/myapp/VPAdapterTeclado.java
@@ -2,18 +2,26 @@ package br.com.etecia.myapp;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class VPAdapterTeclado extends FragmentStateAdapter {
 
+    public VPAdapterTeclado(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return null;
+         switch(position){
+             case 0: return new TecladoFragment();
+             case 1: return new MouseFragment();
+         }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 4;
     }
 }
